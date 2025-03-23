33041f66459e683738e9ad6f235257acb5a49faa
kushal <p.kushal1911@gmail.com>
2025-03-23 12:24:02 +0530

    Updated API and Firebase configuration to use environment variables

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Replace with your Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyBYIDj4oO6Q1SnnZTYsDAlIs2KrWmg2z_k",
-  authDomain: "movie-search-app-525f4.firebaseapp.com",
-  projectId: "movie-search-app-525f4",
-  storageBucket: "movie-search-app-525f4.appspot.com", // Fixed incorrect storage URL
-  messagingSenderId: "564567649133",
-  appId: "1:564567649133:web:5c79101b3055c658338864"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET, // Fixed incorrect storage URL
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
